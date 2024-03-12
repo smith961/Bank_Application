c01726dd63a6938048b46d75eba3c8ea2ae89f48
smith <smithbabawande@gmail.com>
2024-03-12 14:07:39 +0100

    completed

--- Project.java
+++ Project.java
@@ -1,77 +1,95 @@
 package Projects.Project1;
 
+
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.*;
 
+
 public  class Project implements Developers{
 
+    Connection connection = null;
+    private void createTable() throws SQLException {
+        Connection connection1 = connectToDatabase();
+        Statement statement = connection1.createStatement();
+        String createTable = "CREATE TABLE IF NOT EXISTS developers(name Text, age Integer, location Text, skill Text)";
+        statement.execute(createTable);
+        statement.close();
+        closeDatabase();
+    }
 
-    @Override
-    public   ResultSet loadDevelopers(){
-        ResultSet result = null;
-        Connection connection = null;
-        try{
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/developer", "root","123456");
-            Statement statement = connection.createStatement();
+    private void loadFromTextToDB(String fileName){
 
-            //Create Table developers
+        try(BufferedReader reader = new BufferedReader(new FileReader(fileName))){
+            String line;
 
-            String createTable = "CREATE TABLE IF NOT EXISTS developers(id Integer, name Text, age Text, location Text, skill Text)";
-            statement.execute(createTable);
-        }catch (SQLException exception){
+            while( (line = reader.readLine()) != null ){
+                String mode = line.substring(0, line.lastIndexOf('#'));
+                String[] parts = mode.split(",");
+                String name = parts[0].trim();
+                int age = Integer.parseInt(parts[1].trim());
+                String location = parts[2].trim();
+                String skill = parts[3].trim();
+                uploadToDB(name, age, location, skill);
+            }
+
+        }catch (IOException exception){
             System.out.println(exception.getMessage());
-        }finally{
-            if (connection != null){
-                try{
-                    connection.close();
-                }catch (SQLException exception){
-                    System.out.println(exception.getMessage());
-                }
-            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-
-        //Fetch contents of project.txt
-
-         File file = new File("C:\\Users\\E L I T E B O O K\\IdeaProjects\\Ingryd\\src\\Projects\\Project1\\project.txt");
-        BufferedReader reader = null;
-
-        try{
-            reader = new BufferedReader(new FileReader(file));
-            String line = reader.readLine();
-            while( line != null){
-                line = reader.readLine();
-            }
-        }catch(IOException e){
-            System.out.println(e.getMessage());
-        }finally {
-            try {
-                if(reader != null){
-                    reader.close();
-                }
-            }catch (IOException e){
-                System.out.println(e.getMessage());
-            }
-        }
-
-        //populate the table with the fetched content
-        String insertUserSQL1 = "INSERT INTO developers(name, age, location, skill) VALUES()"
-
-
-        //Query statement to fetch the loaded contents
-
-        // return the loaded contents
-        return result;
 
     }
 
+    private void uploadToDB(String name, int age, String location, String skill) throws SQLException{
+        Connection conn = connectToDatabase();
+        Statement statement = conn.createStatement();
+        String uploadSQL = String.format("INSERT INTO developers(name, age, location, skill) VALUES('%S', %d, '%s', '%s')", name, age, location, skill);
+        statement.execute(uploadSQL);
+        statement.close();
+        closeDatabase();
+    }
+
+    private Connection connectToDatabase() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/developer", "root", "123456");
+        return connection;
+    }
+
+    private void closeDatabase() throws SQLException{
+        try{
+            if( connection != null ){
+                connection.close();
+            }
+        } catch (SQLException exception){
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    @Override
+    public ResultSet loadDevelopers() {
+        ResultSet resultSet = null;
+        try{
+            createTable();
+            loadFromTextToDB("C:\\Users\\E L I T E B O O K\\IdeaProjects\\Ingryd\\src\\Projects\\Project1\\project.txt");
+            Connection conn = connectToDatabase();
+            Statement statement = conn.createStatement();
+            String selectStatement = "SELECT * FROM developers";
+            resultSet = statement.executeQuery(selectStatement);
+
+
+        } catch (SQLException exception){
+            System.out.println(exception.getMessage());
+        }
+        return resultSet;
+    }
 
     public static void main(String[] args) {
         Project project1 = new Project();
         project1.loadDevelopers();
     }
+
+
 }
 
 
