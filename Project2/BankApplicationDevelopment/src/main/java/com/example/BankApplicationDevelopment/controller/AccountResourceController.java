370b246e40edf87acfb24e767918ee5b5e53c351
smith <smithbabawande@gmail.com>
2024-04-15 18:37:17 +0100

    project2

--- Project2/BankApplicationDevelopment/src/main/java/com/example/BankApplicationDevelopment/controller/AccountResourceController.java
+++ Project2/BankApplicationDevelopment/src/main/java/com/example/BankApplicationDevelopment/controller/AccountResourceController.java
@@ -6,6 +6,7 @@ import com.example.BankApplicationDevelopment.service.AccountUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,21 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/resource")
 public class AccountResourceController {
-@Autowired
+    @Autowired
     private AccountUserService accountUserService;
 
-@GetMapping("/{id}")
-    public ResponseEntity<AccountResource>getAccountResource(@PathVariable int id){
-    AccountUser accountUserToCreate = accountUserService.getAccountUserById(id).getBody();
-    AccountResource accountResource = new AccountResource();
-    accountResource.setAccountUser(accountUserToCreate);
+    @GetMapping("/{id}")
+    public ResponseEntity<AccountResource> getAccountResource(@PathVariable int id) {
+        AccountUser accountUserToCreate = accountUserService.getAccountUserById(id).getBody();
+        AccountResource accountResource = new AccountResource();
+        accountResource.setAccountUser(accountUserToCreate);
 
-    Link selfLink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(AccountUserController.class).getAccountUserById(id)).withSelfRel();
-    Link delete = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(AccountUserController.class).deleteAccountUser(id)).withRel("delete");
-    Link update = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(AccountUserController.class).updateAccountUser(id,accountUserToCreate)).withRel("update");
-    Link allAccountUser = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(AccountUserController.class).getAllAccountUser()).withRel("allAccountUser");
+        Link selfLink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(AccountUserController.class).getAccountUserById(id)).withSelfRel();
+        Link delete = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(AccountUserController.class).deleteAccountUser(id)).withRel("delete");
+        Link update = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(AccountUserController.class).updateAccountUser(id, accountUserToCreate)).withRel("update");
+        Link allAccountUser = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(AccountUserController.class).getAllAccountUser()).withRel("allAccountUser");
+
+        return new ResponseEntity<>(accountResource, HttpStatus.OK);
 
 
-
-}
+    }
 }
